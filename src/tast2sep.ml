da151c9bd941fb754a3821e4d066ea930432ed3f
tiago <tl.soares@campus.fct.unl.pt>
2023-09-14 10:57:46 +0100

    doc and small debug

--- src/tast2sep.ml
+++ src/tast2sep.ml
@@ -5,9 +5,15 @@ module Env = Map.Make(String)
 open Ttypes
 
 type env = ty Env.t
+
+(** maps variable identifiers with their respective types*)
 let id_table : (string, vsymbol) Hashtbl.t = Hashtbl.create 100
+(** maps the names of generated representation predicates to their respective types.
+   might not be strictly necessary for our translation, but will make the 
+   generated AST easier to manipulate *)
 let pred_table : (string, vsymbol) Hashtbl.t = Hashtbl.create 100
 
+(** maps the names of types with their respective logical models*)
 let env : env ref = ref (Env.empty)
 
 let add f l = env := Env.add f l !env 
@@ -20,21 +26,29 @@ let mk_val s = "_V_" ^ s
 
 let mk_update s =  mk_val (s ^ "'")
 let id_of_term t = match t.Tterm.t_node with  |Tterm.Tvar v -> v |_ -> assert false
+let get_ty_name ty = match ty.ty_node with |Tyapp (t, _) -> t.ts_ident.id_str |_ -> assert false
 
-let mk_pred_vs pred_name self_type = 
+(** creates a {!vs_symbol} for a representation predicate application.
+    For now, this function only works for monomorphic types. 
+    @param pred_name the name for the predicate
+    @param self_type the type that is being lifted*)
+let mk_pred_vs pred_name self_type =
+  let type_name = get_ty_name self_type in
+  let model_type = try get type_name with |Not_found -> self_type in 
   let pred_type = 
     {ty_node = 
-      Tyapp(ts_arrow, [self_type; self_type; ty_bool])} in 
+       Tyapp(ts_arrow, [self_type; model_type; ty_bool])} in
   let id = Ident.create ~loc:Location.none pred_name in 
   let vs = {vs_name = id; vs_ty = pred_type} in 
   let () = Hashtbl.add pred_table pred_name vs in
   vs
 
+(** lifts a variable into its logical model using a generated representation predicate.
+    @param read_only if the variable we are lifting is read_only. If this is set to true, the resulting term will also be read-only
+    @param old this flag is set to false if we are lifting the modified version of the variable in a postcondition. This flag should
+               not be false while {!read_only} is set to true*)
 let mk_pred_app ~read_only ~old arg =
-  let pred_name = 
-    match arg.vs_ty.ty_node with 
-    |Tyapp (t, _) -> get_rep_pred t.ts_ident.id_str 
-    |_ -> assert false in 
+  let pred_name = get_ty_name arg.vs_ty in 
   let arg_name = arg.vs_name.id_str in 
   let arg_val = if not old then mk_update arg_name else mk_val arg_name in 
   let pred_vs = 
@@ -46,7 +60,12 @@ let mk_pred_app ~read_only ~old arg =
 
 open Tterm    
 
-let map_term t is_old = 
+(** Replaces usages of the logical model defined in the GOSPEL specification with the 
+    lifted model from the representation predicate. This function is a bit hacky at the moment
+    since it assumes that there is only one model named "view".
+    @param t the term we want to transform
+    @param is_old should be set to false if this term is from a precondition
+    *)
   let rec map_term t is_old = 
     let f t = map_term t is_old in 
     let map_node = match t.t_node with 
@@ -67,8 +86,8 @@ let map_term t is_old =
       |Tbinop(b, t1, t2) -> Tbinop(b, f t1, f t2)
       |Tnot t -> Tnot (f t)
       |Tfield(t, n) -> Tfield(f t, n)
-      |_ -> t.t_node in {t with t_node=map_node} in 
-  map_term t is_old
+      |_ -> t.t_node in {t with t_node=map_node}
+
 
 let rec signature_item_desc s = match s with 
 |Sig_type(_, l, _) -> List.concat_map (fun t -> type_declaration t) l
@@ -81,7 +100,7 @@ let rec signature_item_desc s = match s with
 
 and val_description des =
   let name = des.vd_name in 
-  let get_arg x = match x with |Lnone x -> x |_-> assert false in
+  let get_arg x = match x with |Lnone x |Loptional x |Lnamed x | Lghost x -> x |_-> assert false in
   let args_vsym = List.map get_arg des.vd_args in
 
   let mk_val_sym is_old v = 
@@ -119,10 +138,7 @@ and val_description des =
   let post_star = mk_sep_term (Star (post_write@post_terms)) in 
   let pre = mk_sep_term (Star (pre_rw@pre_terms)) in 
   let post = mk_sep_term (Exists (modified_val, post_star)) in
-  let () = print_endline "wow" in 
   let post = mk_sep_term (Lambda (ret, post)) in 
-  
-
   
   Triple {
     triple_name = name; 
@@ -139,7 +155,7 @@ and type_declaration t =
     |Some s ->  
       let model_to_arg (sym, _) = 
         let arg = sym.ls_name in
-        let ty = match sym.ls_value with |Some t -> t |None -> assert false in 
+        let ty = match sym.ls_value with |Some t -> t |None -> assert false (*unreachable?*) in 
         let field = {vs_name = arg; vs_ty = ty} in
         let () = add id.id_str ty in 
         field in 

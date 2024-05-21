5fa641da2815069c435b14f92e6ef5c6fd226276
Tiago Soares <tl.soares@campus.fct.unl.pt>
2024-05-21 15:56:08 +0100

    sep logic refactor

--- src/sep_ast.ml
+++ src/sep_ast.ml
@@ -2,33 +2,38 @@ open Ppxlib
 open Tterm
 open Tast
 
+(** Separation Logic terms *)
 type sep_term =
-  | Star of sep_term list
-  | Pure of term
-  | App of Symbols.lsymbol * Symbols.vsymbol list
-  | Magic of sep_term * sep_term
-  | Forall of Symbols.vsymbol list * sep_term
-  | Exists of Symbols.vsymbol list * sep_term
-  | Lambda of Symbols.vsymbol list * sep_term
-  | RO of sep_term
-  | Top
-
-type type_definition = { t_name : Ident.t; t_args : Ident.t list }
+  | Star of sep_term list (** Separating conjuction *)
+  | Pure of term (** Pure term *)
+  | App of Symbols.lsymbol * Symbols.vsymbol list (** Representation predicate application *)
+  | Exists of Symbols.vsymbol list * sep_term (** Existential quantifier *)
+  | Top (** T *)
+  | Magic of sep_term * sep_term (** Magic Wand, unused for now *)
+  | RO of sep_term (** Read only permission, unused for now *)
 
 type triple = {
-  triple_name : Ident.t;
-  triple_args : Symbols.vsymbol option list;
-  triple_vars : Symbols.vsymbol list;
-  triple_pre : sep_term;
-  triple_type : core_type;
-  triple_post : sep_term;
+    triple_name : Ident.t;
+    (** function name *)
+    triple_args : Symbols.vsymbol option list;
+    (** triple_arguments. None when they are unamed (i.e. ()) *)
+    triple_vars : Symbols.vsymbol list;
+    (** Universally quantified variables *)
+    triple_rets : Symbols.vsymbol list;
+    (** Return values *)
+    triple_pre : sep_term;
+    (** Precondition *)
+    triple_type : core_type;
+    (** Function type *)
+    triple_post : sep_term;
+    (** Postcondition *)
 }
-
+(** Top level definitions *)
 type definition_node =
-  | Pred of Ident.t * Symbols.vsymbol list
-  | Type of Ident.t * Ttypes.tvsymbol list
-  | Triple of triple
-  | Axiom of Tast.axiom
-  | Function of Tast.function_
+  | Pred of Ident.t * Symbols.vsymbol list (** Representation Predicate *)
+  | Type of Ident.t * Ttypes.tvsymbol list (** Type definition *)
+  | Triple of triple (** Separation Logic Triples *)
+  | Axiom of Tast.axiom (** Axiom *)
+  | Function of Tast.function_ (** Logical Function *)
 
 type definition = { d_node : definition_node; d_loc : Location.t }

b680106cd1847e6d8bf4c5ce7c0712907fdd5237
Tiago Soares <tl.soares@campus.fct.unl.pt>
2024-11-28 16:25:14 +0100

    Add option to not print types in Gospel terms

--- src/tterm_printer.mli
+++ src/tterm_printer.mli
@@ -11,10 +11,10 @@
 open Tterm
 open Symbols
 
-val print_vs : vsymbol Fmt.t
+val print_vs : print_type:bool -> vsymbol Fmt.t
 val print_ls_decl : lsymbol Fmt.t
 val print_ls_nm : lsymbol Fmt.t
-val print_pattern : pattern Fmt.t
+val print_pattern : print_type:bool -> pattern Fmt.t
 val print_binop : binop Fmt.t
 val print_quantifier : quant Fmt.t
-val print_term : term Fmt.t
+val print_term : print_type:bool -> term Fmt.t

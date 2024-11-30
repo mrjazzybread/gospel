7cc71037d07203deb5af3c34b94de6f41561083e
Tiago Soares <tl.soares@campus.fct.unl.pt>
2024-11-30 17:56:48 +0100

    Add namespace for representation predicates

--- src/sep_utils.mli
+++ src/sep_utils.mli
@@ -11,6 +11,22 @@
 (** Various utility functions to facilitate the translation of Gospel into
     Separation Logic *)
 
+type namespace
+(** Maps strings to their corresponding representation predicates *)
+
+val empty_env : unit -> namespace
+(** The empty namespace *)
+
+val get_pred : namespace -> Id_uast.pty -> Sast.psymbol option
+(** [get_pred m s] returns the representation predicate for the type [s].
+    @raise Not_found
+      If [s] is a type whose logical model is isomorphic to its OCaml
+      representation or [s] is not in scope. *)
+
+val map_pred : namespace -> Ident.Tag.t -> Ident.t -> Id_uast.pty list -> unit
+(** [map_pred m tag s l] maps [tag] to a representation predicate named [s] that
+    takes arguments of type [l]. *)
+
 val change_id : (string -> string) -> Ident.t -> Ident.t
 (** Returns a fresh identifier where the [id_str] field of [change_id f id] is
     equal to [f id.id_str] *)

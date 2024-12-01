c88ac7683561b9147e56439a816e2278c051707c
Tiago Soares <tl.soares@campus.fct.unl.pt>
2024-12-01 12:25:12 +0100

    Sep Logic existential variables inlining

--- src/sep_utilis.ml
+++ src/sep_utilis.ml
@@ -138,3 +138,83 @@ let function_poly f =
 let ty_poly tl =
   let sl = List.map get_ty_poly tl in
   Tv_set.elements (union_all sl)
+
+(* Functions for inlining existential variables *)
+
+let is_var v1 t =
+  match t.t_node with
+  | Tvar v2 -> Ident.equal v1.vs_name v2.vs_name
+  | _ -> false
+
+let check_term v t =
+  match t.t_node with
+  | Tapp (f, [ t1; t2 ]) when ls_equal f ps_equ ->
+      if is_var v t1 then Some t2 else if is_var v t2 then Some t1 else None
+  | Tbinop (Tiff, t1, t2) ->
+      if is_var v t1 then Some t2 else if is_var v t2 then Some t1 else None
+  | _ -> None
+
+let rec map_tvars changed tbl t =
+  let f = map_tvars changed tbl in
+  let t_node =
+    match t.t_node with
+    | Tvar v when Hashtbl.mem tbl v ->
+        let () = changed := true in
+        Hashtbl.find tbl v
+    | Tapp (x, l) -> Tapp (x, List.map f l)
+    | Tif (t1, t2, t3) -> Tif (f t1, f t2, f t3)
+    | Tlet (x, t1, t2) -> Tlet (x, f t1, f t2)
+    | Tcase (t, l) ->
+        Tcase (f t, List.map (fun (x, t1, t2) -> (x, Option.map f t1, f t2)) l)
+    | Tquant (x, y, t) -> Tquant (x, y, f t)
+    | Tlambda (x, t) -> Tlambda (x, f t)
+    | Tbinop (b, t1, t2) -> Tbinop (b, f t1, f t2)
+    | Tnot t -> Tnot (f t)
+    | Told t -> Told (f t)
+    | _ -> t.t_node
+  in
+  { t with t_node }
+
+let rec map_sep_terms tbl t =
+  let changed = ref false in
+  let rec t_map = function
+    | Lift (v, l) ->
+        let l = List.map (map_tvars changed tbl) l in
+        Lift (v, l)
+    | Pure t -> Pure (map_tvars changed tbl t)
+    | Wand (t, l) -> Wand (List.map t_map t, List.map t_map l)
+    | Quant (q, l, s) -> Quant (q, l, List.map t_map s)
+  in
+  if !changed then map_sep_terms tbl (t_map t) else t_map t
+
+let inline (vl, tl) =
+  let tbl = Hashtbl.create 10 in
+  let rec inner_loop v = function
+    | Pure gt :: xs ->
+        let t = check_term v gt in
+        if Option.is_some t then (t, xs)
+        else
+          let t, l = inner_loop v xs in
+          (t, Pure gt :: l)
+    | x :: xs ->
+        let t, l = inner_loop v xs in
+        (t, x :: l)
+    | [] -> (None, [])
+  in
+  let rec loop tl = function
+    | x :: xs -> (
+        let t, tl = inner_loop x tl in
+        match t with
+        | Some t ->
+            let () = Hashtbl.add tbl x t.t_node in
+            let xs, tl = loop tl xs in
+            (xs, tl)
+        | None ->
+            let l, tl = loop tl xs in
+            (x :: l, tl))
+    | [] -> ([], tl)
+  in
+  let vl, tl = loop tl vl in
+  (vl, List.map (map_sep_terms tbl) tl)
+
+let inline_def t = { t with triple_post = inline t.triple_post }

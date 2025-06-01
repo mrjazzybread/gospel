2b936b2f362f77f4ef2cc2569ed730836e174708
Tiago Soares <tl.soares@campus.fct.unl.pt>
2025-06-01 15:04:43 +0200

    Translate type declarations into Separation Logic

---
+++ src/sep_utils.mli
@@ -0,0 +1,20 @@
+(**************************************************************************)
+(*                                                                        *)
+(*  GOSPEL -- A Specification Language for OCaml                          *)
+(*                                                                        *)
+(*  Copyright (c) 2018- The VOCaL Project                                 *)
+(*                                                                        *)
+(*  This software is free software, distributed under the MIT license     *)
+(*  (as described in file LICENSE enclosed).                              *)
+(**************************************************************************)
+
+(** Various utility functions to facilitate the translation of Gospel into
+    Separation Logic *)
+
+val change_id : (string -> string) -> Ident.t -> Ident.t
+(** Returns a fresh identifier where the [id_str] field of [change_id f id] is
+    equal to [f id.id_str] *)
+
+val rep_pred : string -> string
+(** Receives the name of a type and generates the name of its representation
+    predicate *)

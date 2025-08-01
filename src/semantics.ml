7114ca05725e9bfe84c2d414d2820ac58a96d034
Tiago Soares <tl.soares@campus.fct.unl.pt>
2025-08-01 18:01:27 +0100

    Translate Gospel specifications to SL

--- src/semantics.ml
+++ src/semantics.ml
@@ -2,6 +2,16 @@ open Id_uast
 open Tast
 open Sep_utils
 open Sast
+module Env = Ident.IdTable
+
+type spatial_info = {
+  arg_pred : Sast.sep_term;  (** Representation Predicate *)
+  arg_log : tsymbol;  (** Logical representation *)
+  ro : bool;  (** Read only flag *)
+}
+(** Contains the ownership information for a variable within a specification *)
+
+type val_info = { arg_val : triple_val; arg_spatial : spatial_info option }
 
 let cons x l = match x with None -> l | Some x -> x :: l
 
@@ -53,15 +63,132 @@ let type_declaration ~ocaml ns t =
   let () = List.iter (Sep_utils.map_pred ns) spec.ty_lenses in
   cons type_decl (cons model_decl pred_def)
 
+let is_present env v =
+  let id = Uast_utils.leaf v in
+  Env.mem env id.id_tag
+
+(** [map_term env ns is_old t] maps a Gospel specification term into a
+    Separation Logic term. This function first replaces any modified variable
+    outside of an [old] block with the corresponding variable within [env]. It
+    then transforms the Gospel term as is specified in [tterm_to_sep] *)
+let map_term env t =
+  let rec change_vars t =
+    let f t = change_vars t in
+    let map_node =
+      match t.t_node with
+      | Tvar v when (not (Uast_utils.leaf v).id_old) && is_present env v ->
+          Tvar (Env.find env (Uast_utils.leaf v).id_tag)
+      | Tlet (v, t1, t2) -> Tlet (v, f t1, f t2)
+      | Told t -> (f t).t_node
+      | Tapply (t1, t2) -> Tapply (f t1, f t2)
+      | Tif (t1, t2, t3) -> Tif (f t1, f t2, f t3)
+      | Tquant (q, l, t) -> Tquant (q, l, f t)
+      | Tfield (t, n) -> Tfield (f t, n)
+      | Tscope (q, t) -> Tscope (q, f t)
+      | _ -> t.t_node
+    in
+    { t with t_node = map_node }
+  in
+  Logical (change_vars t)
+
+let update_var s = "_" ^ s ^ "'"
+
+let map_id env is_old nm =
+  let needs_updated_var = (not is_old) && is_present env nm in
+  let id = Uast_utils.leaf nm in
+  let id' = if needs_updated_var then change_id update_var id else id in
+
+  Env.add env id.id_tag (Qid id');
+  id'
+
+let to_prog arg =
+  let arg_id = Uast_utils.leaf arg.var_name in
+  mk_ts arg_id arg.ty_ocaml
+
+type which = Arg_old | Arg_update | Ret
+
+let lifted_arg ~which ns env arg =
+  match arg with
+  | (Id_uast.Unit | Wildcard | Ghost _) when which = Arg_update -> None
+  | Id_uast.Unit -> Some { arg_spatial = None; arg_val = Unit }
+  | Wildcard -> Some { arg_spatial = None; arg_val = Wildcard }
+  | Ghost (id, ty) ->
+      Some { arg_spatial = None; arg_val = Ghost { ts_id = id; ts_ty = ty } }
+  | OCaml v ->
+      let ro = v.ro in
+      let arg_log = map_id env (which = Arg_old || ro) v.var_name in
+      let arg_typ, lens =
+        if which = Arg_old then v.ty_gospel_cons else v.ty_gospel_prod
+      in
+      let arg_ts = mk_ts arg_log arg_typ in
+      let pred = get_pred ns lens.lens_desc in
+      let arg_prog = to_prog v in
+      let to_term v = mk_term (Tvar (Qid v.ts_id)) v.ts_ty Location.none in
+      let arg_spatial =
+        match which with
+        | Arg_update when ro || pred.ps_persistent -> None
+        | _ ->
+            let arg_pred = Lift (pred, to_term arg_prog, to_term arg_ts) in
+            Some { arg_pred; arg_log = arg_ts; ro }
+      in
+      let arg_val =
+        Sast.Value
+          {
+            arg_ocaml = arg_prog;
+            arg_model = arg_ts;
+            is_loc = Uast_utils.can_own v.ty_ocaml;
+          }
+      in
+      Some { arg_spatial; arg_val }
+
+let val_des des spec ns =
+  let env = Env.create 100 in
+  let lift ~which = List.filter_map (lifted_arg ~which ns env) in
+  let args = (lift ~which:Arg_old) spec.sp_args in
+  let update_args = (lift ~which:Arg_update) spec.sp_args in
+  let rets = (lift ~which:Ret) spec.sp_rets in
+
+  let pre = List.map (map_term env) spec.sp_pre in
+  let post = List.map (map_term env) spec.sp_post in
+
+  let lifts l =
+    List.filter_map (fun x -> Option.map (fun x -> x.arg_pred) x.arg_spatial) l
+  in
+  let triple_pre = lifts args @ pre in
+  let triple_post = lifts (update_args @ rets) @ post in
+  let mk_updates arg =
+    Option.bind arg.arg_spatial (fun x -> if x.ro then None else Some x.arg_log)
+  in
+  let updated_vars = List.filter_map mk_updates update_args in
+  let ret_vars = List.filter_map mk_updates rets in
+  let triple_post = (updated_vars @ ret_vars, triple_post) in
+  (* Gets the program value for the argument. *)
+  let to_prog_arg = fun arg -> arg.arg_val in
+  let def =
+    {
+      triple_name = des.vname;
+      triple_args = List.map to_prog_arg args;
+      triple_rets = List.map to_prog_arg rets;
+      triple_pre;
+      triple_poly = des.vtvars;
+      triple_post;
+    }
+  in
+  Triple def
+
 (** Transforms a single Gospel top level declaration into potentially several
     Separation Logic definitions *)
-let rec signature_item_desc ns = function
+let signature_item_desc ns = function
   | Tast.Sig_type l -> List.concat_map (type_declaration ns ~ocaml:true) l
   | Sig_ghost_type l -> List.concat_map (type_declaration ns ~ocaml:false) l
   | Sig_function f -> [ Function f ]
+  | Sig_value v -> (
+      match v.vspec with
+      | Some spec -> [ Val v; val_des v spec ns ]
+      | None -> [ Val v ])
   | _ -> []
 
-and signature_item ns s =
+let signature_item ns s =
   let sigs = signature_item_desc ns s.sdesc in
   let sigs = List.map (fun sep -> { d_node = sep; d_loc = s.sloc }) sigs in
   sigs

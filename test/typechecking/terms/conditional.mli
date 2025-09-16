e43665e355bce934234c9d2c6d2598fde8a20264
Tiago <tl.soares@campus.fct.unl.pt>
2025-09-16 16:26:33 +0100

    If statements are guarded by propositions

--- test/typechecking/terms/conditional.mli
+++ test/typechecking/terms/conditional.mli
@@ -3,9 +3,9 @@
       then None
       else Some (Sequence.hd s) *)
 (* {gospel_expected|
-[1] File "conditional.mli", line 2, characters 9-27:
+[1] File "conditional.mli", line 2, characters 22-27:
     2 |       if s = Sequence.empty
-                 ^^^^^^^^^^^^^^^^^^
-    Error: Mismatch between type bool and type prop
+                              ^^^^^
+    Error: Unbound value Sequence.empty
     
 |gospel_expected} *)

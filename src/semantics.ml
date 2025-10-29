a0fb435c1c1b9f5114e57915e6a6555e3bd380f4
Tiago Soares <tl.soares@campus.fct.unl.pt>
2025-10-29 10:04:43 +0000

    SL semantics uses lenses

--- src/semantics.ml
+++ src/semantics.ml
@@ -13,27 +13,10 @@ type spatial_info = {
 
 type val_info = { arg_val : triple_val; arg_spatial : spatial_info option }
 
-let ty_loc =
-  let app = Uast_utils.mk_info (Qid (Ident.mk_id "loc")) in
-  PTtyapp (app, [])
-
 let cons x l = match x with None -> l | Some x -> x :: l
 
 (** Translates a Gospel type declaration into 1-3 Separation Logic definitions.
-    These are as follows:
-
-    - If the model has at least one named field, generates a type definition for
-      a record that will serve as the model for the type where each field has
-      the same name and type as the Gospel model. In this case, the record type
-      will have the same name as the original OCaml type.
-
-    - If the type is not labelled as ephemeral, generates a type declaration for
-      the OCaml type. If the type has any named model fields, then the name of
-      this type will be the name of the OCaml type with an '_' at the start.
-
-    - If the type has a model that is not isomorphic to its OCaml type,
-      generates an abstract representation predicate that lifts the OCaml value
-      into a logical representation *)
+*)
 let type_declaration ~ocaml ns t =
   (* Creates a type declaration for the model. If the model has no
      named model fields, then this function returns None *)
@@ -53,46 +36,9 @@ let type_declaration ~ocaml ns t =
     | _ -> None
   in
 
-  (* Generates a representation predicate in which the first argument
-     is the OCaml value and the second is its logical model. If the
-     logical model is isomorphic to the OCaml type, then it returns
-     [None] *)
-  let pred_def model_type is_mutable tvar_list =
-    let app = Uast_utils.mk_info (Qid t.tname) in
-    let prog_ty = PTtyapp (app, tvar_list) in
-    let pred_prog_ty = if is_mutable then ty_loc else prog_ty in
-
-    let prog_vs = mk_ts (Ident.mk_id "target") pred_prog_ty in
-
-    let pred_model_ty =
-      match model_type with
-      | No_model _ -> pred_prog_ty
-      | Implicit (_, t) -> t
-      | Fields _ -> PTtyapp (app, tvar_list)
-    in
-    let model_vs = mk_ts (Ident.mk_id "model") pred_model_ty in
-    let new_id = change_id rep_pred t.tname in
-    let pred_type = [ pred_prog_ty; pred_model_ty ] in
-    let () = Sep_utils.map_pred ns t.tname.id_tag new_id is_mutable pred_type in
-    let pred_kind = if is_mutable then Heap else Pure in
-    Pred
-      {
-        pred_name = new_id;
-        pred_args = [ prog_vs; model_vs ];
-        pred_poly = t.tparams;
-        pred_kind;
-      }
-  in
-
   let spec = t.tspec in
-  let tvar_list = List.map (fun x -> PTtyvar x) t.tparams in
   let model_type = spec.ty_model in
   let is_record = match model_type with Fields _ -> true | _ -> false in
-  let is_mutable =
-    match spec.ty_model with
-    | No_model m | Implicit (m, _) -> m = Parse_uast.Mutable
-    | Fields l -> List.exists (fun x -> x.pld_mutable = Mutable) l
-  in
   let type_name =
     if is_record then change_id (( ^ ) "_") t.tname else t.tname
   in
@@ -113,11 +59,9 @@ let type_declaration ~ocaml ns t =
     else None
   in
   let model_decl = model_decl model_type in
-  let pred_def =
-    if ocaml then Some (pred_def model_type is_mutable tvar_list) else None
-  in
-
-  cons type_decl (cons model_decl (cons pred_def []))
+  let pred_def = List.map (fun x -> Pred x) spec.ty_lenses in
+  let () = List.iter (Sep_utils.map_pred ns) spec.ty_lenses in
+  cons type_decl (cons model_decl pred_def)
 
 let is_present env v =
   let id = Uast_utils.leaf v in
@@ -171,8 +115,11 @@ let lifted_arg ns env is_pre arg =
   | OCaml v ->
       let ro = v.ro in
       let arg_log = map_id env (is_pre || ro) v.var_name in
-      let arg_ts = mk_ts arg_log (fst v.ty_gospel_prod) in
-      let pred = get_pred ns v.ty_ocaml in
+      let arg_typ, lens =
+        if is_pre then v.ty_gospel_cons else v.ty_gospel_prod
+      in
+      let arg_ts = mk_ts arg_log arg_typ in
+      let pred = get_pred ns lens.lens_desc in
       let arg_prog = to_prog v in
       let to_term v = mk_term (Tvar (Qid v.ts_id)) v.ts_ty Location.none in
       let arg_spatial =
